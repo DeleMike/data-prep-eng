04caca4a5c0d87e20d06a0696a5c69f79d0f33ac
DeleMike <akindelemichael65@gmail.com>
2023-11-17 10:15:11 +0000

    rename procedures and file

--- data_prep_eng/__main__.py
+++ data_prep_eng/__main__.py
@@ -1,9 +1,9 @@
-from data_prep_eng.prepare import create_new_dataset
+from data_prep_eng.prepare_menyo20k import create_new_menyo_dataset
 from data_prep_eng.prepare_bible import combine_all_bible_data
 
 def main():
-   create_new_dataset()
-   create_new_dataset(type_of_dataset='test')
+   create_new_menyo_dataset()
+   create_new_menyo_dataset(type_of_dataset='test')
    # test_prepare()
    combine_all_bible_data()
 
